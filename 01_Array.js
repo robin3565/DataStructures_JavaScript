dc44aee510e4ac7c465d7d7a14b52216b043c516
Robin <dbtjs8858@gmail.com>
2022-09-11 00:00:09 +0900

    Update Example

--- 01_Array.js
+++ 01_Array.js
@@ -39,36 +39,36 @@ class Array {
     }
 }
 
-// const array= new Array(); //we are instantiating an object of Array class
-//     array.push(12);
-//     array.push(13); //pushing element 
-//     array.push(14);
-//     array.push(10);
-//     array.push(989);
-//     document.write("<div>Print element in an array</div>");
-//     for(var key in array.data){
-//       document.write("<span>"+array.data[key]+" "+"</span>"); 
-//     }
+const array= new Array(); //we are instantiating an object of Array class
+    array.push(12);
+    array.push(13); //pushing element 
+    array.push(14);
+    array.push(10);
+    array.push(989);
+    document.write("<div>Print element in an array</div>");
+    for(var key in array.data){
+      document.write("<span>"+array.data[key]+" "+"</span>"); 
+    }
       
       
-//     document.write("<div>Pop element in an array</div>");
-//     array.pop(); //Popping element 989
-//     for(var key in array.data){
-//       document.write("<span>"+array.data[key]+" "+"</span>"); 
-//     }
+    document.write("<div>Pop element in an array</div>");
+    array.pop(); //Popping element 989
+    for(var key in array.data){
+      document.write("<span>"+array.data[key]+" "+"</span>"); 
+    }
       
       
-//     document.write("<div>Inserting element at position 2</div>");
-//     array.insertAt(456, 2); //Inserting element 456
-//     for(var key in array.data){
-//       document.write("<span>"+array.data[key]+" "+"</span>"); 
-//     }
+    document.write("<div>Inserting element at position 2</div>");
+    array.insertAt(456, 2); //Inserting element 456
+    for(var key in array.data){
+      document.write("<span>"+array.data[key]+" "+"</span>"); 
+    }
       
-//     document.write("<div>deleting element at position 3</div>");
-//     array.deleteAt(3); //Deleting 14
-//     for(var key in array.data){
-//       document.write("<span>"+array.data[key]+" "+"</span>"); 
-//     }
+    document.write("<div>deleting element at position 3</div>");
+    array.deleteAt(3); //Deleting 14
+    for(var key in array.data){
+      document.write("<span>"+array.data[key]+" "+"</span>"); 
+    }
       
-//     document.write("<div>Getting  element at position 2</div>");
-//     document.write("<div>"+array.getElementAtIndex(2)+"</div>");
+    document.write("<div>Getting  element at position 2</div>");
+    document.write("<div>"+array.getElementAtIndex(2)+"</div>");
